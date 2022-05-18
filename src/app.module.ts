c861a8ffdd47a4d8d6b6544836a9fec9b1bca554
sad7898 <siradhee_k@hotmail.com>
2022-05-19 00:47:28 +0700

    preconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://admin:<password>@cluster0.q1gam.mongodb.net/?retryWrites=true&w=majority`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
