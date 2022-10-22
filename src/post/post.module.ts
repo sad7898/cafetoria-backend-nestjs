9bbf7e7de713d3a35b71d89a05907dd19348c29a
siradheek <siradhee_k@hotmail.com>
2022-10-22 21:51:56 +0200

    add throttling

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -4,9 +4,17 @@ import { PostController } from './post.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Post, PostSchema } from './entities/post.entity';
 import { UserModule } from 'src/user/user.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]), UserModule],
+  imports: [
+    MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+    UserModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 30,
+    }),
+  ],
   controllers: [PostController],
   providers: [PostService],
 })
