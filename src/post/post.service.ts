9e3cd3171ed9bdb925a3ece07b08b63b00af9873
siradheek <siradhee_k@hotmail.com>
2022-10-22 20:49:06 +0200

    implement like

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,14 +1,10 @@
 import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import mongoose, { FilterQuery, Model } from 'mongoose';
+import { Model } from 'mongoose';
 import { UserService } from 'src/user/user.service';
-import { BulkPostResponse, CreatePostDto, PostFilterDto, PostQuery, PostResponse, UpdatePostDto } from './dto/post.dto';
+import { BulkPostResponse, CreatePostDto, PostFilterDto, UpdatePostDto } from './dto/post.dto';
 import { Post, PostDocument } from './entities/post.entity';
 import { FilterBuilder } from './filter';
-import { Filter } from './post.interface';
-import { faker } from '@faker-js/faker';
-import { NotFoundError } from 'rxjs';
-import { pipeline } from 'stream';
 
 @Injectable()
 export class PostService {
@@ -16,34 +12,41 @@ export class PostService {
   async create(createPostDto: CreatePostDto, userId: string) {
     const author = await this.userService.findById(userId);
     if (!author) throw new BadRequestException('User not found');
-    return await this.postModel.create({ ...createPostDto, author });
+    const createdPost = await this.postModel.create({ ...createPostDto, author });
+    this.userService.update({ createdPosts: [createdPost] }, userId);
+    return createdPost;
   }
 
   async findAll({ tags, topic, sortKey }: PostFilterDto, page: number): Promise<BulkPostResponse> {
     if (sortKey && !['created', 'topic'].includes(sortKey)) throw new BadRequestException('Invalid sort key');
     const filterBuilder = new FilterBuilder<PostDocument>().addMatchAll('tags', tags).addTextSearch(topic);
     const filter = filterBuilder.getFilter();
-    const query = this.postModel.aggregate<BulkPostResponse>().match(filter).lookup({
-      from: "users",
-      localField: "author",
-      foreignField: "_id",
-      as: "author",
-      pipeline: [{
-        $project: {
-          "_id":1,
-          "name":1
-        }
-      }]
-    })
-    const filterHasTextSearch = filter.$and?.filter((f) => f.$text).length > 0
+    const query = this.postModel
+      .aggregate<BulkPostResponse>()
+      .match(filter)
+      .lookup({
+        from: 'users',
+        localField: 'author',
+        foreignField: '_id',
+        as: 'author',
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              name: 1,
+            },
+          },
+        ],
+      });
+    const filterHasTextSearch = filter.$and?.filter((f) => f.$text).length > 0;
     if (filterHasTextSearch) query.sort({ score: { $meta: 'textScore' } });
     else query.sort({ [sortKey || 'created']: sortKey === 'created' ? -1 : 1 });
-    const groupOption: any = {_id:null,posts: {$push: "$$ROOT"},count: {$sum:1}}
+    const groupOption: any = { _id: null, posts: { $push: '$$ROOT' }, count: { $sum: 1 } };
     query.group(groupOption).project({
       count: 1,
-      posts: {$slice: ["$posts",page*10,10]}
-    })
-    const res = await query
+      posts: { $slice: ['$posts', page * 10, 10] },
+    });
+    const res = await query;
     return res[0];
   }
   async findById(id: string) {
@@ -76,6 +79,13 @@ export class PostService {
     if (!post) throw new BadRequestException('This post does not exist');
     if (post.author._id.toString() !== authorId) throw new ForbiddenException("You don't have permission to modify this post.");
     return await post.updateOne({ ...updatePostDto, author: post.author, created: new Date() });
+  }
+  async updateLikeCount(postId: string, likerId: string) {
+    const post = await this.postModel.findById(postId);
+    if (!post) throw new BadRequestException('This post does not exist');
+    this.userService.update({ likedPosts: [post] }, likerId);
+    post.likeCount += 1;
+    return await post.save();
   }
 
   async remove(id: string, authorId: string) {
